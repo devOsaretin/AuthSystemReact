4463aced82e2b6ddb78e2e842b443f02ab9c8325
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-23 12:38:37 +0100

    chore: setup app pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { login } from "./features/auth/authSlice";
+
 function App() {
+	const token = useAppSelector(state => state.auth.token);
 	return (
 		<div>
 			<h2>Authentication System</h2>
